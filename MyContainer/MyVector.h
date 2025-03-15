eeea78841348309abc35db04082d137a0e453694
Modin <eom3658@gmail.com>
2025-03-15 23:28:11 +0900

    MyVector 함수 선언

---
+++ MyContainer/MyVector.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <memory>
+
+/*
+ * 동적 배열
+ * - 배열은 선언과 동시에 크기가 제한된다.
+ * - 그러나 동적 배열은 수용량을 초과할 때, 그 이상을 담을 수 있는 배열을 새로 만들어 기존 배열을 복사한다.
+ */
+
+template <typename T>
+class MyVector
+{
+
+public:
+	MyVector(size_t capacity = 16);
+	MyVector(const MyVector& rhs);
+	MyVector(MyVector&& rhs);
+	~MyVector();
+
+	T* begin();
+	T* end();
+
+	T& front();
+	T& back();
+
+	size_t size();
+	size_t capacity();
+	bool empty();
+	void reserve(size_t capacity);
+
+	void push_back(const T& value);
+	void pop_back();
+	void clear();
+};
