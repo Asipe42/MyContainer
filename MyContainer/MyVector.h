c9feb31d93fd28629f6a7e038d4621bfe0012bb1
Modin <eom3658@gmail.com>
2025-03-15 23:33:10 +0900

    MyVector 복사 생성자 구현

--- MyContainer/MyVector.h
+++ MyContainer/MyVector.h
@@ -48,7 +48,20 @@ inline MyVector<T>::MyVector(size_t capacity)
 
 template<typename T>
 inline MyVector<T>::MyVector(const MyVector& rhs)
+	: m_size(rhs.m_size)
+	, m_capacity(rhs.m_capacity)
+	, m_arr(new T[rhs.m_capacity])
 {
+	/*
+	 * 복사 생성자
+	 *	- 깊은 복사를 수행한다.
+	 *	- 깊은 복사란 포인터가 참조하고 있는 메모리에 있는 데이터에 대한 사본을 만드는 것이다.
+	 */
+
+	for (size_t i = 0; i < m_size; i++)
+	{
+		m_arr[i] = rhs.m_arr[i];
+	}
 }
 
 template<typename T>
