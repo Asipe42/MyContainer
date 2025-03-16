1e57e10351772bd4229e37090aadcd733b4ebb07
Modin <eom3658@gmail.com>
2025-03-16 16:44:26 +0900

    MyList 이동 생성자 구현 및 복사 생성자 개선

--- MyContainer/MyStack.h
+++ MyContainer/MyStack.h
@@ -36,24 +36,32 @@ inline MyStack<T>::MyStack()
 
 template<typename T>
 inline MyStack<T>::MyStack(const MyStack& rhs)
+	: m_list(rhs.m_list)
+	, m_size(rhs.m_size)
 {
 	/*
 	 * 복사 생성자
 	 *	- 깊은 복사를 수행한다.
 	 *	- 깊은 복사란 포인터가 참조하고 있는 메모리에 있는 데이터에 대한 사본을 만드는 것이다.
 	 */
-
-	m_list(rhs.m_list);
-	m_size = rhs.m_size;
 }
 
 template<typename T>
-inline MyStack<T>::MyStack(MyStack&& rhs)
+inline MyStack<T>::MyStack(MyStack&& rhs) noexcept
+	: m_list(std::move(rhs.m_list))
+	, m_size(rhs.m_size)
 {
+	/*
+	* 이동 생성자
+	*	- 이동 생성자는 rvalue에 대한 사본을 생성한다.
+	*  - rvalue는 임시 객체를 의미한다.
+	*/
+
+	rhs.m_size = 0;
 }
 
 template<typename T>
-inline MyStack<T>::~MyStack()
+inline MyStack<T>::~MyStack() noexcept
 {
 }
 
@@ -70,19 +78,19 @@ inline MyStack<T>& MyStack<T>::operator=(MyStack&& rhs)
 }
 
 template<typename T>
-inline bool MyStack<T>::empty()
+inline bool MyStack<T>::empty() const
 {
 	return false;
 }
 
 template<typename T>
-inline size_t MyStack<T>::size()
+inline size_t MyStack<T>::size() const
 {
 	return size_t();
 }
 
 template<typename T>
-inline T& MyStack<T>::top()
+inline T& MyStack<T>::top() const
 {
 	// TODO: 여기에 return 문을 삽입합니다.
 }
