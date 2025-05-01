b390dbe760040ef769c45eb4f4fa5185af2af07f
Modin <eom3658@gmail.com>
2025-05-01 16:45:42 +0900

    MyMap 함수 선언

--- MyContainer/MyMap/MyMap.h
+++ MyContainer/MyMap/MyMap.h
@@ -1,8 +1,14 @@
 ﻿#pragma once
 
+template <typename K, typename V>
 class MyMap
 {
 public:
+    class MyIterator
+    {
+        
+    };
+    
     MyMap() noexcept;
     MyMap(const MyMap& rhs);
     MyMap(MyMap&& rhs) noexcept;
@@ -10,28 +16,105 @@ public:
 
     MyMap& operator=(const MyMap& rhs);
     MyMap& operator=(MyMap&& rhs) noexcept;
+
+    V& operator[](const K& key);
+    void insert(K key, V value);
+    void erase(K key);
+    void erase(MyIterator it);
+    
+    MyIterator find(K key);
+    bool count(K key) const;
+
+    void clear();
+    size_t size() const;
+    bool empty() const;
+
+    class MyIterator;
+    MyIterator begin();
+    MyIterator end();
 };
 
-inline MyMap::MyMap() noexcept
+template <typename K, typename V>
+MyMap<K, V>::MyMap() noexcept
 {
 }
 
-inline MyMap::MyMap(const MyMap& rhs)
+template <typename K, typename V>
+MyMap<K, V>::MyMap(const MyMap& rhs)
 {
 }
 
-inline MyMap::MyMap(MyMap&& rhs) noexcept
+template <typename K, typename V>
+MyMap<K, V>::MyMap(MyMap&& rhs) noexcept
 {
 }
 
-inline MyMap::~MyMap() noexcept
+template <typename K, typename V>
+MyMap<K, V>::~MyMap() noexcept
 {
 }
 
-inline MyMap& MyMap::operator=(const MyMap& rhs)
+template <typename K, typename V>
+MyMap<K, V>& MyMap<K, V>::operator=(const MyMap& rhs)
 {
 }
 
-inline MyMap& MyMap::operator=(MyMap&& rhs) noexcept
+template <typename K, typename V>
+MyMap<K, V>& MyMap<K, V>::operator=(MyMap&& rhs) noexcept
+{
+}
+
+template <typename K, typename V>
+V& MyMap<K, V>::operator[](const K& key)
+{
+}
+
+template <typename K, typename V>
+void MyMap<K, V>::insert(K key, V value)
+{
+}
+
+template <typename K, typename V>
+void MyMap<K, V>::erase(K key)
+{
+}
+
+template <typename K, typename V>
+void MyMap<K, V>::erase(MyIterator it)
+{
+}
+
+template <typename K, typename V>
+typename MyMap<K, V>::MyIterator MyMap<K, V>::find(K key)
+{
+}
+
+template <typename K, typename V>
+bool MyMap<K, V>::count(K key) const
+{
+}
+
+template <typename K, typename V>
+void MyMap<K, V>::clear()
+{
+}
+
+template <typename K, typename V>
+size_t MyMap<K, V>::size() const
+{
+}
+
+template <typename K, typename V>
+bool MyMap<K, V>::empty() const
+{
+}
+
+template <typename K, typename V>
+typename MyMap<K, V>::MyIterator MyMap<K, V>::begin()
+{
+}
+
+template <typename K, typename V>
+typename MyMap<K, V>::MyIterator MyMap<K, V>::end()
 {
 }
